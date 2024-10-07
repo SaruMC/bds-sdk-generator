29881562fabb054e6f9328c041e2377a3408191d
Alexis Daugé <alexis.dauge2004@gmail.com>
2024-10-07 16:46:29 +0200

    loading offset, rva and more

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <llvm/Demangle/Demangle.h>
 
 #include "Logger.h"
+#include "PDBUtils.h"
 
 #include <filesystem>
 #include <iostream>
@@ -8,20 +9,33 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-     if (argc != 2) {
-         cerr << "Usage: " << argv[0] << " <filename>" << endl;
-         return EXIT_FAILURE;
-     }
+  if (argc != 2) {
+    cerr << "Usage: " << argv[0] << " <filename>" << endl;
+    return EXIT_FAILURE;
+  }
 
-    string fileName = argv[1];
+  if (const string fileName = argv[1];
+      !filesystem::exists(fileName) || fileName.size() <= 4 || fileName.substr(fileName.size() - 4) != ".pdb") {
+    Logger::error("The given file is either not existsing or not a .pdb file");
+    return EXIT_FAILURE;
+  }
 
-    if (!filesystem::exists(fileName) || fileName.size() <= 4 || fileName.substr(fileName.size() - 4) != ".pdb") {
-        Logger::error("The given file is either not existsing or not a .pdb file");
-        return EXIT_FAILURE;
-    }
+  Logger::info("Loading symbols from given pdb file...");
+  PDBUtils pdbUtils;
+  const auto symbols = pdbUtils.loadPdb();
 
-    Logger::info("Loading symbols from given pdb file...");
-    // auto syms = loadPDBFile(fileName);
+  if (!symbols) {
+    Logger::error("Failed to load symbols from the pdb file");
+    return EXIT_FAILURE;
+  }
 
-    return EXIT_SUCCESS;
+  Logger::info("Symbols loaded successfully!");
+
+  for (const auto& symbol : *symbols) {
+    cout << "Symbol: " << symbol->value << " | RVA: " << symbol->rva << " | Offset: " << symbol->offset << endl;
+  }
+
+  delete symbols;
+
+  return EXIT_SUCCESS;
 }
