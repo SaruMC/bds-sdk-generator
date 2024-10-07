af05adad051ee44c11c5b639349f042c69c24327
Alexis Daugé <alexis.dauge2004@gmail.com>
2024-10-07 15:44:18 +0200

    better like this

--- include/Logger.h
+++ include/Logger.h
@@ -11,15 +11,15 @@ private:
     static const std::string PREFIX;
 
 public:
-    static void info(std::string text) {
+    static void info(const std::string& text) {
         std::cout << "\033[33m" << PREFIX << " - [INFO]: " << text << std::endl;
     }
 
-    static void error(std::string text) {
+    static void error(const std::string& text) {
         std::cerr << "\033[31m" << PREFIX << " - [ERROR]: " << text << std::endl;
     }
 
-    static void debug(std::string text) {
+    static void debug(const std::string& text) {
         std::cout << "\033[32m" << PREFIX << " - [DEBUG]: " << text << std::endl;
     }
 };
